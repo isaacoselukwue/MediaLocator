bee0345261071f7cdf6be7b2260e5907cec0daab
Isaac Chukwuemeke Helge Oselukwue <princeizak@live.com>
2025-01-29 19:49:18 +0000

    code implementation test to automate issues board resolution #95692740

---
+++ ML.Application/Common/Models/Result.cs
@@ -0,0 +1,15 @@
+﻿namespace ML.Application.Common.Models;
+public class Result
+{
+    internal Result(bool succeeded, string message, IEnumerable<string> errors)
+    {
+        Succeeded = succeeded;
+        Message = message;
+        Errors = errors.ToArray();
+    }
+    public bool Succeeded { get; init; }
+    public string Message { get; init; }
+    public string[] Errors { get; init; }
+    public static Result Success(string message) => new(true, message, []);
+    public static Result Failure(string message, IEnumerable<string> errors) => new(false, message, errors);
+}
