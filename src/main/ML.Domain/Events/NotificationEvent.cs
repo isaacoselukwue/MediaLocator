e2d8faef929733e1533c32dff8644bb3684c4cfe
Isaac Chukwuemeke Helge Oselukwue <princeizak@live.com>
2025-02-03 00:57:24 +0000

    setup of the email notification consumer, setup of signin manager, set up of token generation
    Add ML.Service project, JWT, email, and HTTP resilience
    
    - Added new ML.Service project with worker service and MassTransit consumer
    - Updated MediaLocator.sln to include ML.Service project
    - Replaced IMediator with ISender in AuthenticationController.cs
    - Added RequestId header in CustomResponseHeaderMiddleware.cs
    - Introduced JWT settings in appsettings.Development.json and appsettings.json
    - Added project reference to ML.Domain in ML.Application.csproj
    - Added MediatR package reference in ML.Domain.csproj
    - Introduced HTTP resilience and email services in DependencyInjection.cs
    - Added necessary package references in ML.Infrastructure.csproj
    - Implemented IEmailService and IIdentityService interfaces
    - Added BaseEvent and NotificationEvent classes in ML.Domain
    - Added NotificationTypeEnum enum in ML.Domain.Enums
    - Implemented EmailService using MailKit in ML.Infrastructure.Email
    - Added MailSettings class for email configuration
    - Created IdentityResultExtensions and IdentityService in ML.Infrastructure.Identity
    - Implemented JwtService for JWT token management
    - Added JwtSettings class for JWT configuration
    - Updated launchSettings.json to include ML.Service profile
    - Added Worker class in ML.Service for background tasks
    - Created NotificationConsumer class in ML.Service.Consumers
    #13

---
+++ src/main/ML.Domain/Events/NotificationEvent.cs
@@ -0,0 +1,12 @@
+﻿using ML.Domain.Common;
+using ML.Domain.Enums;
+
+namespace ML.Domain.Events;
+public class NotificationEvent(string receiver, string subject, NotificationTypeEnum notificationType,
+    Dictionary<string, string> replacements) : BaseEvent
+{
+    public string Receiver { get; } = receiver;
+    public string? Subject { get; } = subject;
+    public Dictionary<string, string>? Replacements { get; } = replacements;
+    public NotificationTypeEnum NotificationType { get; } = notificationType;
+}
